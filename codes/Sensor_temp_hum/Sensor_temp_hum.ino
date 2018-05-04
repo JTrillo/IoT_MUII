5e526f4128e081e26e5db8d230f8638323e05416
JTrillo <33926302+JTrillo@users.noreply.github.com>
2018-05-04 18:06:26 +0200

    DHT (library and code)

---
+++ codes/Sensor_temp_hum/Sensor_temp_hum.ino
@@ -0,0 +1,40 @@
+#include "DHT.h"
+
+//#define DHTTYPE DHT11   // DHT 11
+#define DHTTYPE DHT22   // DHT 22  (AM2302), AM2321
+//#define DHTTYPE DHT21   // DHT 21 (AM2301)
+const int DHTPin = 5;     // what digital pin we're connected to
+ 
+DHT dht(DHTPin, DHTTYPE);
+
+void setup() {
+   Serial.begin(9600);
+   Serial.println("DHTxx test!");
+ 
+   dht.begin();
+}
+
+void loop() {
+  // Wait a few seconds between measurements.
+   delay(2000);
+ 
+   // Reading temperature or humidity takes about 250 milliseconds!
+   float h = dht.readHumidity();
+   float t = dht.readTemperature();
+ 
+   if (isnan(h) || isnan(t)) {
+      Serial.println("Failed to read from DHT sensor!");
+      return;
+   }
+ 
+ 
+   Serial.print("Humidity: ");
+   Serial.print(h);
+   Serial.println(" %");
+   Serial.print("Temperature: ");
+   Serial.print(t);
+   Serial.println(" *C ");
+  
+}
+
+
